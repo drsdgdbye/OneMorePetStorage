df6ae1f5dfe4971f5f20f62d66ebde4d89d3fded
drsdgdby <nolifeirl@2ch.hk>
2019-04-08 20:26:17 +0300

    .

--- client/src/main/java/sample/Main.java
+++ client/src/main/java/sample/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
+        primaryStage.setTitle("Authorization");
         primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
